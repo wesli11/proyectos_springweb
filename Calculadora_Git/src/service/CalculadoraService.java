faca9b7bd0e34bd8ef2fe2507d89dddf378fe7c9
pili <pili@pili-HP-Pavilion-dv6-Notebook-PC>
2021-12-29 15:58:50 +0100

    version 1

---
+++ Calculadora_Git/src/service/CalculadoraService.java
@@ -0,0 +1,8 @@
+package service;
+
+public interface CalculadoraService {
+
+	public int sumar(int a, int b);
+	public int restar(int a, int b);
+	
+}
---
+++ Calculadora_Git/src/service/CalculadoraServiceImpl.java
@@ -0,0 +1,17 @@
+package service;
+
+public class CalculadoraServiceImpl implements CalculadoraService{
+
+	@Override
+	public int sumar(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public int restar(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+}
